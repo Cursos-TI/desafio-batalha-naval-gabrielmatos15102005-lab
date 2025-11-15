35384eb925fef8a0417172a5db71cb94d48bd717
gabrielmatos15102005-lab <gabrielmatos15102005@gmail.com>
2025-11-15 01:01:18 +0000

    Navio vertical

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,7 +5,20 @@ int main() {
 
     
     int navioVertical[3][2];
+    int navioHorizontal[3][2];
 
+   // Posicionamento dos Navios
+
+
+    // Navio vertical
+
+    navioVertical[0][0] = 2;  navioVertical[0][1] = 1;
+    navioVertical[1][0] = 2;  navioVertical[1][1] = 2;
+    navioVertical[2][0] = 2;  navioVertical[2][1] = 3;
+
+    // Navio horizontal
+
+    
 
     return 0;
 }
